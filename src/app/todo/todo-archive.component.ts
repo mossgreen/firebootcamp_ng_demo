6e74ba63cd56de7d9d64c364e6194cb43582b0d8
Feifei GU <gufeifei@outlook.com>
2016-12-27 17:17:27 +1300

    Add the Todo Archive component

---
+++ src/app/todo/todo-archive.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {Todo} from './todo.model';
+import {TodoService} from './todo.service';
+
+@Component({
+    selector:'todo-archive',
+    template:
+    `
+    <div>
+        <h2>Archived Todos</h2>
+        <div>
+            <todo-list [todos]="todos">
+        </div>
+    </div>
+    `,
+    styles:['a {cursor:pointer; cursor: hand;}']
+})
+
+export class TodoArchiveComponent implements OnInit{
+    todos: Todo[];
+    constructor(private _todoService: TodoService){}
+    ngOnInit(){
+        this.todos = this._todoService.get().filter(todo => todo.archived === true)
+    }
+}
